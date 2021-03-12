5138db8fe25cac7b61f62fb21177b7e9f3ea4d6e
Arijit Saha <arijitbiley@gmail.com>
2021-03-12 19:25:28 +0530

    Add View component for landing page.

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/Layout";
+import View from "../components/View";
+import Status from "../components/Status";
+
+const Index = () => (
+  <Layout>
+    <Status />
+    <View title="AWS Lambda Task Scheduler App">
+      <p>This is a task scheduler that helps you schedule Lambda functions.</p>
+      <p>
+        This is made as a CWoD project by team <code>AWS-T12</code>, whose
+        members are <code>Arijit Saha</code> and <code>Rohit Kumar</code>.
+      </p>
+      <p>
+        For the full experience, go to
+        {` `}
+        <Link to="/app/tasks">your tasks</Link>.
+      </p>
+    </View>
+  </Layout>
+);
+
+export default Index;
