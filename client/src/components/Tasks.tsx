2d95ed725f449c432bfee9089302524ed047fced
Arijit Saha <arijitbiley@gmail.com>
2021-03-13 09:58:37 +0530

    Add task page to private routes

---
+++ client/src/components/Tasks.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import View from "./View";
+import { getCurrentUser } from "../services/auth";
+
+const Tasks = () => {
+  const currentUser = getCurrentUser();
+
+  if (!("email" in currentUser)) {
+    return null;
+  }
+
+  const { name } = currentUser;
+
+  return (
+    <View title="Tasks">
+      <p>Welcome to your tasks, {name}!</p>
+      <p>
+        This is a client-only route. You could set up a form to save information
+        about a user here.
+      </p>
+    </View>
+  );
+};
+
+export default Tasks;
