2a424262f3f5f411d3047828ad371966f96b7376
Arijit Saha <arijitbiley@gmail.com>
2021-03-14 12:41:48 +0530

    Create Task Schema

---
+++ server/models/Task.ts
@@ -0,0 +1,37 @@
+import { Schema, model, Model, Document } from "mongoose";
+
+// Interface for Task Schema
+export interface TaskDocument extends Document {
+  _id: string;
+  taskURL: string;
+  delayInMS: number;
+  status: "scheduled" | "running" | "completed" | "failed" | "cancelled";
+}
+
+// For model type
+export interface TaskModelInterface extends Model<TaskDocument> {}
+
+// create schema for Task
+const TaskSchema = new Schema<TaskDocument, TaskModelInterface>(
+  {
+    taskURL: {
+      type: String,
+      required: [true, "The Task URL is required"],
+    },
+    delayInMS: {
+      type: Number,
+      required: [true, "The Task delay is required"],
+    },
+    status: {
+      type: String,
+      enum: ["scheduled", "running", "completed", "failed", "cancelled"],
+      default: "scheduled",
+    },
+  },
+  { emitIndexErrors: true, timestamps: true }
+);
+
+// create model for Tasks
+const TaskModel: Model<TaskDocument> = model("Task", TaskSchema, "Tasks");
+
+export default TaskModel;
